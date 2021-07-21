9f1d5fc879a10a2800674ea9fc9b94968686223a
datt7e4 <datt7e4@gmail.com>
2021-07-20 17:58:41 -0700

    add nav-bar

---
+++ client/src/shared/components/Navbar/MenuItems.js
@@ -0,0 +1,30 @@
+export const MenuItems = [
+  {
+    title: "Coins",
+    url: "coins",
+  },
+  {
+    title: "Dex",
+    url: "dex",
+  },
+  {
+    title: "Market Place",
+    url: "marketplace",
+  },
+
+  {
+    title: "Tools",
+    url: "tools",
+  },
+
+  {
+    title: "About US",
+    url: "about-us",
+  },
+
+  {
+    title: "Sign up",
+    url: "sign-up",
+  },
+];
+export const BrandName = "BlockPhilio";
